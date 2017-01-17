be107394583b29f5937e63000463d591369c89a4
Hyungro Lee <hroe.lee@gmail.com>
2017-01-17 13:47:48 -0500

    fall 15 domain

---
+++ README.rst
@@ -0,0 +1,7 @@
+bd_stats_from_classes
+===============================================================================
+
+Big Data Statistics from IU Classes are provided. 
+
+
+
--- stats.py
+++ stats.py
@@ -33,9 +33,61 @@ class bdStatsClasses(object):
         self.result = res
         return res
 
+    def create_domain_json(self, dname):
+        tmp = {}
+        for i in self.raw_data:
+            name = i['Dataset']
+            url = i['Dataset URL']
+            tmp[name] = url
+
+        res = {
+                dname: {
+                    'categories': {
+                        'bigdataclass': tmp
+                        }
+                    }
+                }
+
+        return res
+
+    def save_json(self, filename, data):
+        with open(filename, 'w') as f:
+            json.dump(data, f)
+        f.close()
+
+    def stats_two(self):
+        """for fall 15 csv files"""
+        """where id, title, technology*, user interface language, backend
+        environment, dataset, github url, dataset url"""
+
+        m = 0
+        c = Counter()
+        for i in self.raw_data:
+            items = i['Technology*']
+            item = items.split("\n")
+            item = [ x.lower() for x in item ]
+            m = self.mean([m, len(item)])
+            c2 = Counter(item)
+            c += c2
+
+            lang = i['User Interface Language']
+            env = i['Backend Environment**']
+            data = i['Dataset URL']
+
+        res = { 
+                "techonologies": c,
+                'total_count': len(self.raw_data),
+                'average_roles': m
+                }
+        self.result = res
+        return res
+
+
 if __name__ == "__main__":
 
     stat = bdStatsClasses()
     stat.load_csv(sys.argv[1])
-    res = stat.stats()
-    pprint (res)
+    #res = stat.stats()
+    #pprint (res)
+    res = stat.create_domain_json('fall15')
+    stat.save_json('fall15.json', res)
